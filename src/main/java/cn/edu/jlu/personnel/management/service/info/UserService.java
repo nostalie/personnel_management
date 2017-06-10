053d6ecba05ec3a2aee99c766cead478da6d72bf
nostalie.zhang <nostalie.zhang@qunar.com>
2017-06-10 20:09:23 +0800

    修正查询的部门查询

--- src/main/java/cn/edu/jlu/personnel/management/service/info/UserService.java
+++ src/main/java/cn/edu/jlu/personnel/management/service/info/UserService.java
@@ -1,11 +1,13 @@
 package cn.edu.jlu.personnel.management.service.info;
 
 import cn.edu.jlu.personnel.management.dao.UserDao;
+import cn.edu.jlu.personnel.management.enums.Auth;
 import cn.edu.jlu.personnel.management.support.Read;
 import cn.edu.jlu.personnel.management.util.EncryptUtil;
 import cn.edu.jlu.personnel.management.vo.model.Department;
 import cn.edu.jlu.personnel.management.vo.model.Position;
 import cn.edu.jlu.personnel.management.vo.model.User;
+import cn.edu.jlu.personnel.management.web.interceptor.IdentityInterceptor;
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
@@ -34,53 +36,64 @@ public class UserService {
 
     /**
      * 由用户名查询用户信息
+     *
      * @param userName
      * @return
      */
     @Read
-    public Optional<User> queryUserInfo(String userName){
+    public Optional<User> queryUserInfo(String userName) {
         try {
             Preconditions.checkNotNull(userName);
             User result = userDao.selectUserByUserName(userName);
-            if(result != null) {
+            if (result != null) {
                 result.setPassword(EncryptUtil.decode(result.getPassword()));
                 fillUser(result);
             }
             return Optional.fromNullable(result);
         } catch (Exception e) {
-            LOGGER.error("查询用户信息失败，userName is: {}",userName,e);
+            LOGGER.error("查询用户信息失败，userName is: {}", userName, e);
             throw new RuntimeException(e);
         }
     }
 
     @Read
-    public Optional<User> queryUserById(Integer userId){
+    public Optional<User> queryUserById(Integer userId) {
         try {
             Preconditions.checkNotNull(userId);
             User result = userDao.selectUserByUserId(userId);
-            if(result != null){
+            if (result != null) {
                 result.setPassword(EncryptUtil.decode(result.getPassword()));
                 fillUser(result);
             }
             return Optional.fromNullable(result);
         } catch (Exception e) {
-            LOGGER.error("查询用户信息失败，userId is: {}",userId,e);
+            LOGGER.error("查询用户信息失败，userId is: {}", userId, e);
             throw new RuntimeException(e);
         }
     }
+
     /**
      * 按条件查询结果集
+     *
      * @param user
      * @return
      */
     @Read
-    public List<User> queryUsers(User user, RowBounds rowBounds){
+    public List<User> queryUsers(User user, RowBounds rowBounds) {
         try {
             Preconditions.checkNotNull(user);
-            if(user.getPassword() != null) {
+            Integer auth = IdentityInterceptor.AUTH.get();
+            if (Auth.isContain(auth, Auth.BU_LEADER) && !Auth.isContain(auth, Auth.SUPER_LEADER)) {
+                String requestUserName = IdentityInterceptor.USER_NAME.get();
+                User requestUser = queryUserInfo(requestUserName).isPresent() ? queryUserInfo(requestUserName).get() : null;
+                if (requestUser != null) {
+                    user.setDepartmentId(requestUser.getDepartmentId());
+                }
+            }
+            if (user.getPassword() != null) {
                 user.setPassword(EncryptUtil.encode(user.getPassword()));
             }
-            return Lists.transform(userDao.selectUsers(user,rowBounds), new Function<User, User>() {
+            return Lists.transform(userDao.selectUsers(user, rowBounds), new Function<User, User>() {
                 public User apply(User user) {
                     user.setPassword(EncryptUtil.decode(user.getPassword()));
                     fillUser(user);
@@ -88,55 +101,55 @@ public class UserService {
                 }
             });
         } catch (Exception e) {
-            LOGGER.error("安条件查询用户失败，查询条件为: {}",user);
+            LOGGER.error("安条件查询用户失败，查询条件为: {}", user);
             throw new RuntimeException(e);
         }
     }
 
-    private void fillUser(User user){
+    private void fillUser(User user) {
         Preconditions.checkNotNull(user);
         Optional<Department> departmentOptional = departmentService.queryDepartmentById(user.getDepartmentId());
         Optional<Position> positionOptional = positionService.queryPositionById(user.getPositionId());
-        if(departmentOptional.isPresent()){
+        if (departmentOptional.isPresent()) {
             user.setDepartment(departmentOptional.get());
         }
-        if(positionOptional.isPresent()){
+        if (positionOptional.isPresent()) {
             user.setPosition(positionOptional.get());
         }
     }
 
-    public int deleteUser(String userName){
+    public int deleteUser(String userName) {
         try {
             Preconditions.checkNotNull(userName);
             return userDao.deleteUserByUserName(userName);
         } catch (Exception e) {
-            LOGGER.error("删除用户失败，userName is: {}",userName);
+            LOGGER.error("删除用户失败，userName is: {}", userName);
             throw new RuntimeException(e);
         }
     }
 
-    public int addUser(User user){
+    public int addUser(User user) {
         try {
             Preconditions.checkNotNull(user);
-            if(user.getPassword() != null) {
+            if (user.getPassword() != null) {
                 user.setPassword(EncryptUtil.encode(user.getPassword()));
             }
             return userDao.insertUser(user);
         } catch (Exception e) {
-            LOGGER.error("添加用户失败，user is: {}",user);
+            LOGGER.error("添加用户失败，user is: {}", user);
             throw new RuntimeException(e);
         }
     }
 
-    public int updateUser(User user){
+    public int updateUser(User user) {
         try {
             Preconditions.checkNotNull(user);
-            if(user.getPassword() != null){
+            if (user.getPassword() != null) {
                 user.setPassword(EncryptUtil.encode(user.getPassword()));
             }
             return userDao.updateUser(user);
         } catch (Exception e) {
-            LOGGER.error("更新用户失败,user is: {}",user);
+            LOGGER.error("更新用户失败,user is: {}", user);
             throw new RuntimeException(e);
         }
     }
